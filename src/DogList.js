7f43cc0e092373031c2e98efa9fc0e433ea6c9c5
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-08 14:45:01 +0000

    Add DogList component

---
+++ src/DogList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const DOGS = [
+  { id: 1, name: 'Jackie', breed: 'German Shepherd', description: 'Best dog ever', image: 'german-shepherd.jpg' },
+  { id: 2, name: 'Joey', breed: 'Spaniel', description: 'So lovable', image: 'spaniel.jpg' },
+  { id: 3, name: 'Ice', breed: 'Dobermann', description: 'Loves to exercise', image: 'dobermann.jpg' },
+]
+
+export class DogList extends Component {
+  render() {
+    let dogs = DOGS;
+    return (
+      <div id="dog-list">
+        {
+            dogs.map((dog) => (
+              <Dog
+                key={dog.id}
+                name={dog.name}
+                breed={dog.breed}
+                description={dog.description}
+                image={dog.image}
+              />
+            ))
+        }
+      </div>
+    );
+  }
+}
