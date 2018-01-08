ced2c170cf4dbe262067f2433e49de672f046efe
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2018-01-08 12:13:56 +0000

    Adds app.js with two practice routes

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/users', (req, res) => {
+  res.status(200).send({
+    hello: "goodbye"
+  })
+})
+
+app.get('*', (req, res) => res.status(200).send({
+  message: "Welcome to Majd's world"
+}));
+
+app.listen(3000, () => {
+  console.log('hello majd')
+});
+
+module.exports = app;
