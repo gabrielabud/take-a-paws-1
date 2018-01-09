129a5b9c3cbbc86fda3139ec70c23661e1708917
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2018-01-09 12:15:22 +0000

    Adds passport set up

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const env = require('dotenv').load();
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));  /* Check making it true */
+
+app.use(session({ secret: 'take-a-paws', resave: true, saveUninitialized:true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./server/routes')(app);
 
