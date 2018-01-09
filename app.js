a5022fa611b66fbece2399a7e6cba45b5249f64f
Andrew Davey <ajdavey8@gmail.com>
2018-01-09 15:55:43 +0000

    Add cloud storage for images

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cloudinary = require('cloudinary');
+const fileParser = require('connect-multiparty')();
+require('dotenv').config()
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret
+})
 
 const app = express();
 app.use(logger('dev'));
@@ -8,6 +17,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
+
+app.post('/upload', fileParser, function(req, res){
+  var imageFile = req.files.image;
+  cloudinary.uploader.upload(imageFile.path);
+  res.status(200).send({
+    message: "Welcome to Majd's world"
+  })
+});
 
 app.get('*', (req, res) => res.status(200).send({
   message: "Welcome to Majd's world"
