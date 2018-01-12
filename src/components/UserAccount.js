ef91d459af2afa06043b4691f77331e276cf9915
Ignacio Panebianco <ignaciopanebianco@gmail.com>
2018-01-12 14:15:59 +0000

    Adds UserAccount page

---
+++ src/components/UserAccount.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class UserAccount extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      userData: []
+    }
+  }
+
+  componentDidMount() {
+    let self=this;
+    const id = sessionStorage.getItem('id');
+
+    fetch(`http://localhost:3001/users/${id}`)
+      .then(function(results) {
+        return results.json();
+      })
+      .then(function(data){
+          console.log(data)
+          self.setState({
+            userData: data
+          })
+      })
+      .catch(function(error) {
+        console.log(error)
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      <p>{this.state.userData.firstname}</p>
+      <p>{this.state.userData.lastname}</p>
+      <p>{this.state.userData.username}</p>
+      <p>{this.state.userData.email}</p>
+      </div>
+    );
+  }
+}
+export default UserAccount;
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import DogProfile from './components/DogProfile';
 import SignUpForm from './components/SignUpForm';
 import DogForm from './components/DogForm';
 import SignInForm from './components/SignInForm';
+import UserAccount from './components/UserAccount';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 const Routes = (props) => (
@@ -14,6 +15,7 @@ const Routes = (props) => (
       <Route exact path="/signup" component={SignUpForm} />
       <Route exact path="/dogform" component={DogForm} />
       <Route exact path="/signin" component={SignInForm} />
+      <Route exact path="/useraccount" component={UserAccount} />
     </Switch>
   </Router>
 );
