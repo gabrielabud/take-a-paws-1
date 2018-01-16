a1eac5ef8561e91df572818d516d2571fa308656
majdeddine <majda-jambouri@hotmail.fr>
2018-01-16 10:57:41 +0000

    show conversation by sender name

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -21,28 +21,19 @@ sender(req.body.senderId).then(function(user){
   senderName = user;
   sender(req.body.receiverId).then(function(user){
     receiverName = user;
-  Message.create({
-    message: req.body.message,
-    sender: req.body.senderId,
-    receiver: req.body.receiverId,
-    senderName: senderName,
-    receiverName: receiverName
+      Message.create({
+        message: req.body.message,
+        sender: req.body.senderId,
+        receiver: req.body.receiverId,
+        senderName: senderName,
+        receiverName: receiverName
 
-  })
+      })
   .then(message => res.status(201).send(message))
   .catch(error => res.status(400).send(error));
+  })
 })
-})
-    // return Message
-      // .create({
-      //   // message: senderName(req.body.senderId),
-      //   message: req.body.message,
-      //   sender: req.body.senderId,
-      //   receiver: req.body.receiverId
-      //
-      // })
-      // .then(message => res.status(201).send(message))
-      // .catch(error => res.status(400).send(error));
+
   },
 
   list(req, res) {
