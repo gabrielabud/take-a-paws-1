a162f6a1def51cb1641ee2983a744cce3efdd5e0
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-16 11:27:51 +0000

    Update Welcome page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     return (
       <div className="home">
         <div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p>Welcome to Paws world </p>
         </div>
         <div className="home-bg"></div>
       </div>
